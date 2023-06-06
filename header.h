2e05dac6a7295d0966bff06545b5a1280613a31a
avendl5 <133960560+avendl5@users.noreply.github.com>
2023-06-06 10:20:08 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,25 +1,29 @@
-#ifndef HEADER_H
-#define HEADAER_H
+#ifndef Header_H
+#define Header_H
 
-
-
-typedef struct artikl {
-	int id;
-	char ime[50];
-	int cijena;
-
-	
-	
-
+typedef struct artikl {//2,3
+	float cijena;//1
+	char ime_artikla[25];//1,4
+	char raspolozivost[3];//1
+	int id;//1
 }ARTIKL;
 
-void ispis_izbornika();
-void dodaj_artikl();
-void citaj_artikle();
-void ispisi_artikle();
-void pronadi_artikl();
-void brisanje_artikala();
+int izbornik();
+int izbornikSrch();
+int izbornikSrt();
+int makro();
+void kreiranjeDat();
+void dodavanjeArtikala();
+void prepravljanje();
+void* ucitavanjeArtikala();
+void ispisivanje(ARTIKL*);
+void* SearchImeArtikla(ARTIKL*);
+void* SearchCijena(ARTIKL*);
+void subs(ARTIKL*, ARTIKL*);
+void subs2(ARTIKL*, ARTIKL*);
+void selectionSortCijenaUzl(ARTIKL*);
+void selectionSortCijenaSil(ARTIKL*);
+void brisanjeArtikala(ARTIKL*);
+void izlaz(ARTIKL*);
 
-
-
-#endif
+#endif //Header_H
